83415f8415fae2659508590ecac8dd15ec66685c
Ryan Wendell <Ryan.wendell@me.com>
2022-07-15 10:24:59 -0700

    Added the edit form

--- controllers/places.js
+++ controllers/places.js
@@ -19,6 +19,8 @@ router.get('/:id', (req, res) => {
   }
 })
 
+
+
 router.delete('/:id', (req, res) => {
   let id = Number(req.params.id)
   if (isNaN(id)) {
@@ -30,6 +32,19 @@ router.delete('/:id', (req, res) => {
   else {
     places.splice(id, 1)
     res.redirect('/places')
+  }
+})
+
+router.get('/:id/edit', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+      res.render('error404')
+  }
+  else if (!places[id]) {
+      res.render('error404')
+  }
+  else {
+    res.render('places/edit', { place: places[id], id })
   }
 })
 
