2c2c271987c7e790adcc5db4022c250e6ec21228
Ryan Wendell <Ryan.wendell@me.com>
2022-07-20 16:00:29 -0700

    added error message for adding place

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -2,11 +2,23 @@ const React = require('react')
 const Def = require('../default')
 
 function show (data) {
-  console.log(data.id)
     return (
         <Def>
           <main>
             <div className='flexbox'>
+              
+              <div className='image'>
+                <img src= {data.place.pic} alt= {data.place.name} />
+                <h3>Locate in {data.place.city}, {data.place.state}</h3>
+              </div>
+
+              <div className='description'>
+              <h1>{data.place.name}</h1>
+              <h2>Rating</h2>
+              <p>Not Rated</p>
+              <h2>Description</h2>
+              <h3>{data.place.showEstablished()}</h3>
+              <h4> Serving {data.place.cuisines}</h4>
               <div className='buttons'>
                 <a href={`/places/${data.id}/edit`} className='btn btn-warning'>
                   Edit
@@ -17,16 +29,6 @@ function show (data) {
                   </button>
                 </form> 
               </div>
-              <div className='image'>
-                <img src= {data.place.pic} alt= {data.place.name} />
-              </div>
-
-              <div className='description'>
-              <h1>{data.place.name}</h1>
-              <h2>Rating</h2>
-              <p>Not Rated</p>
-              <h2>Description</h2>
-              <p>Locate in {data.place.city}, {data.place.state} and serving {data.place.cuisines}</p>
               </div>
 
               <div className='comments'>
